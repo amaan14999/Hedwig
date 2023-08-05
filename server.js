66fa648c2c3c0a7141f40b02633977e8023e4298
Amaan <amaan14999@gmail.com>
2023-08-06 00:37:41 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname + "/public")));
+
+io.on("connection", function (socket) {
+  socket.on("sender-join", function (data) {
+    socket.join(data.uid);
+  });
+
+  socket.on("receiver-join", function (data) {
+    socket.join(data.uid);
+    socket.in(data.sender_uid).emit("init", data.uid);
+  });
+
+  socket.on("file-meta", function (data) {
+    socket.in(data.uid).emit("fs-meta", data.metadata);
+  });
+
+  socket.on("fs-start", function (data) {
+    socket.in(data.uid).emit("fs-share", {});
+  });
+
+  socket.on("file-raw", function (data) {
+    socket.in(data.uid).emit("fs-share", data.buffer);
+  });
+});
+
+server.listen(5000, function () {
+  console.log("Server is running on port 5000");
+});
